71862c58b9870ee6ef3c2b23784718f659298da1
Karar Shah <karar.shah2015@gmail.com>
2025-02-06 17:17:10 +0500

    added fast api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/py/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/py/:path*"
+            : "/api/py/:path*", // Proxy to Backend
+      },
+    ];
+  },
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
---
+++ src/app/assis/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import OpenAI from "openai";
+
+export default function page() {
+  return (
+    <div className="bg-gray-50 flex items-center justify-center ">
+      <div>Assistant Page</div>
+    </div>
+  );
+}
